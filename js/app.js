999ed394515b690376c3ba27dc63c0d680affd00
Kathy Dieser <katdiese@gmail.com>
2015-05-11 22:22:22 -0600

    modified fizzbuzz

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
 console.log("document ready!");
-    for (var i = 0; i <= 100; i++) {
-      if (i%3 === 0 && i%5 === 0) {
-      $(".list").append("<li>fizzbuzz</li>");}
-      else if (i%3 === 0) {
-      $(".list").append("<li>fizz</li>");}
-      else if (i%5 === 0) {
-      $(".list").append("<li>buzz</li>");}
-      else {
-      $(".list").append("<li>" + i + "</li>");}
+  var i = 0;
+  $("#my-button").click(function(e){
+    if (i%3 === 0 && i%5 === 0) 
+    {
+      $(".list").append("<li>fizzbuzz</li>");
     }
+      else if (i%3 === 0) {       
+      $(".list").append("<li>fizz</li>");
+      }
+      else if (i%5 === 0) {
+      $(".list").append("<li>buzz</li>");
+      }
+      else {
+      $(".list").append("<li>" + i + "</li>");
+      }
+    if (i > 100){
+      alert("You have reached 100, start over!");
+      $(".list").html("");
+      i=0;
+    }
+  i++;
+  });
+    
   
   });
